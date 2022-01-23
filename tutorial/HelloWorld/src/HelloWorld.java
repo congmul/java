5155ad7e008d610b037e787fffe022e57b99830d
Jehyun <congmul@hotmail.com>
2022-01-23 09:33:17 -0800

    Update terms

--- tutorial/HelloWorld/src/HelloWorld.java
+++ tutorial/HelloWorld/src/HelloWorld.java
@@ -1,3 +1,10 @@
+/**
+ * Code block: {}
+ * Access modifier: public, protect, private,
+ * Return type: void, int, String, ...,
+ */
+
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
